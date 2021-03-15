78b1bf047291c351b56f5add95880a96895cbde3
Jihyo Kim <wlgy33@naver.com>
2021-03-15 23:11:13 +0900

    Create ShowMyPics repository and upload
    2021.03.15
    Create Sign In View containing ID, PW, Buttons

---
+++ app/src/main/java/android/example/mystagram/EmailSignUpActivity.kt
@@ -0,0 +1,11 @@
+package android.example.mystagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_sign_up)
+    }
+}
